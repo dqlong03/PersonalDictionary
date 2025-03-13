fb2f768ccb0a4cea9be5c49f1e056796e45485e1
dqlong03 <quanglongduong29@gmail.com>
2025-03-14 00:02:19 +0700

    fix update user

--- PersonalDictionaryProject/Dtos/UpdateUserDTO.cs
+++ PersonalDictionaryProject/Dtos/UpdateUserDTO.cs
@@ -2,9 +2,10 @@
 {
     public class UpdateUserDTO
     {
-        public string Id { get; set; }
+        public string Id { get; set; } 
         public string FullName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public string Password { get; set; } 
     }
 }
