0eb4cf7f6cbbf5729ddc8f3c1c1a24580b0e40e5
Duong Quang Long <145981011+dqlong03@users.noreply.github.com>
2025-03-18 03:58:30 +0700

    Add files via upload

---
+++ PersonalDictionaryProject/Dtos/WordDTO.cs
@@ -0,0 +1,14 @@
+﻿using PersonalDictionaryProject.Models;
+
+namespace PersonalDictionaryProject.Dtos
+{
+    public class WordDTO
+    {
+        public int Id { get; set; }
+        public string WordText { get; set; }
+        public string Definition { get; set; }
+        public string Example { get; set; }
+        public string Language { get; set; }
+        public bool IsPublic { get; set; }
+    }
+}
