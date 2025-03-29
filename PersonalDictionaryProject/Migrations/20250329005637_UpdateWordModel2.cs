cfc0d4ec7ec7d63934797b58c355735ae95acbd3
dqlong03 <quanglongduong29@gmail.com>
2025-03-29 08:02:21 +0700

    fix

---
+++ PersonalDictionaryProject/Migrations/20250329005637_UpdateWordModel2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalDictionaryProject.Migrations
+{
+    public partial class UpdateWordModel2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsApprovedYet",
+                table: "Words",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsApprovedYet",
+                table: "Words",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+    }
+}
